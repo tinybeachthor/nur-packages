0e7d38fd08d794583457608f72e74b90bca1cd91
Martin Toman <toman.martin@live.com>
2020-10-07 20:58:00 +0200

    Add cabbage

--- default.nix
+++ default.nix
@@ -15,6 +15,7 @@
   overlays = import ./overlays; # nixpkgs overlays
 
   example-package = pkgs.callPackage ./pkgs/example-package { };
+  cabbage = pkgs.callPackage ./pkgs/cabbage { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
