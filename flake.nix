ccaa61774400ba28cf7d01ea5b84084f4d26d12b
Martin Toman <toman.martin@live.com>
2020-10-17 23:35:01 +0200

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,10 @@
+{
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-20.09;
+  };
+  outputs = { self, nixpkgs }: {
+    packages.x86_64-linux = import ./default.nix {
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    };
+  };
+}
