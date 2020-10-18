f57add6251e7d1f84163ca92f96472427bd9e42e
Martin Toman <toman.martin@live.com>
2020-10-19 01:05:00 +0200

    Add vrod to vimPlugins

--- pkgs/vimPlugins/default.nix
+++ pkgs/vimPlugins/default.nix
@@ -5,4 +5,5 @@ with { buildVimPluginFrom2Nix = pkgs.vimUtils.buildVimPluginFrom2Nix; };
 {
   distilled-vim = pkgs.callPackage ./distilled-vim { inherit buildVimPluginFrom2Nix; };
   vim-racket = pkgs.callPackage ./vim-racket { inherit buildVimPluginFrom2Nix; };
+  vrod = pkgs.callPackage ./vrod { inherit buildVimPluginFrom2Nix; };
 }
