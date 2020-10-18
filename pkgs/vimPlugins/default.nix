1109e24ff91149604d7094420566c2747c80acd4
Martin Toman <toman.martin@live.com>
2020-10-19 01:00:24 +0200

    Add vim-racket to vimPlugins

--- pkgs/vimPlugins/default.nix
+++ pkgs/vimPlugins/default.nix
@@ -4,4 +4,5 @@ with pkgs;
 with { buildVimPluginFrom2Nix = pkgs.vimUtils.buildVimPluginFrom2Nix; };
 {
   distilled-vim = pkgs.callPackage ./distilled-vim { inherit buildVimPluginFrom2Nix; };
+  vim-racket = pkgs.callPackage ./vim-racket { inherit buildVimPluginFrom2Nix; };
 }
