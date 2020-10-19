eb5c98224952989d91c1ff0898c1029a22984e3a
Martin Toman <toman.martin@live.com>
2020-10-19 13:03:12 +0200

    Add overlay

--- flake.nix
+++ flake.nix
@@ -1,10 +1,9 @@
 {
-  inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-20.09;
-  };
   outputs = { self, nixpkgs }: {
-    packages.x86_64-linux = import ./default.nix {
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    overlay = final: prev: {
+      nur = import ./default.nix {
+        pkgs = prev;
+      };
     };
   };
 }
