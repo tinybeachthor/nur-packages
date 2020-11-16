3b9cae765ca543534c10010851cd5584da8712bf
Martin Toman <toman.martin@live.com>
2020-11-16 13:52:42 +0100

    Add pkgs/libraries/libpd

---
+++ pkgs/libraries/libpd/default.nix
@@ -0,0 +1,36 @@
+{ stdenv
+, fetchFromGitHub
+, autoPatchelfHook
+}:
+
+stdenv.mkDerivation rec {
+  pname = "libpd";
+  version = "0.11.0";
+
+  src = fetchFromGitHub {
+    owner = "libpd";
+    repo = pname;
+    rev = version;
+    sha256 = "sha256-04pSzNGum65Y+IXG6THroWXEAIwVXs7hGTY5H1MLj60=";
+    fetchSubmodules = true;
+  };
+
+  nativeBuildInputs = [ autoPatchelfHook ];
+
+  buildPhase = ''
+    make libpd
+
+    mkdir $out
+    mkdir $out/lib
+    cp libs/libpd.so $out/lib/
+  '';
+
+  dontInstall = true;
+
+  meta = with stdenv.lib; {
+    description = "Pure Data embeddable audio synthesis library";
+    homepage = "https://github.com/libpd/libpd";
+    license = licenses.bsd3;
+    platforms = with platforms; linux;
+  };
+}
