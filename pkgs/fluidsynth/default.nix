1e33c2f8981d24e399313e67be918f621b46e115
Martin Toman <toman.martin@live.com>
2020-12-07 20:49:00 +0100

    Add fluidsynth (2.1.5)

---
+++ pkgs/fluidsynth/default.nix
@@ -0,0 +1,33 @@
+{ stdenv, lib, fetchFromGitHub
+, pkgconfig, cmake
+, glib, libsndfile, libpulseaudio, libjack2
+, alsaLib
+, AudioUnit, CoreAudio, CoreMIDI, CoreServices
+}:
+
+stdenv.mkDerivation rec {
+  pname = "fluidsynth";
+  version = "2.1.5";
+
+  src = fetchFromGitHub {
+    owner = "FluidSynth";
+    repo = "fluidsynth";
+    rev = "v${version}";
+    sha256 =  "sha256-48AF8FfRSBKVofc3/u/mFoNUIB0vmDSeHeUFGC7BlzE=";
+  };
+
+  nativeBuildInputs = [ pkgconfig cmake ];
+
+  buildInputs = [ glib libsndfile libpulseaudio libjack2 ]
+    ++ lib.optionals stdenv.isLinux [ alsaLib ]
+    ++ lib.optionals stdenv.isDarwin [ AudioUnit CoreAudio CoreMIDI CoreServices ];
+
+  cmakeFlags = [ "-Denable-framework=off" ];
+
+  meta = with lib; {
+    description = "Real-time software synthesizer based on the SoundFont 2 specifications";
+    homepage    = "http://www.fluidsynth.org";
+    license     = licenses.lgpl21Plus;
+    platforms   = platforms.unix;
+  };
+}
